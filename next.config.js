388cdc307b8378ebfd21a0a9c0c069959cc67763
iMAGRAY <89709267+iMAGRAY@users.noreply.github.com>
2023-05-13 20:32:35 +0300

    Update next.config.js
    Cryptobiote

--- next.config.js
+++ next.config.js
@@ -4,6 +4,24 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+  // Add runtime caching with network-first strategy
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "https-calls",
+        networkTimeoutSeconds: 15,
+        expiration: {
+          maxEntries: 150,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
+    },
+  ],
 });
 
 const nextConfig = withPWA({
@@ -18,6 +36,8 @@ const nextConfig = withPWA({
       // https://github.com/vercel/next.js/issues/7755#issuecomment-937721514
       config.resolve.fallback.fs = false;
     }
+    // Define the main fields to be used for module resolution.
+    // This can help in cases where certain modules have different entry points defined in their package.json
     config.resolve.mainFields = ["browser", "main", "module"];
     return config;
   },
